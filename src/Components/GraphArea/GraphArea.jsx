8cb7af50f816c30d58079878716ba58f1afab2e1
Aniruddha Ghosh <ghoshaniruddha2003@gmail.com>
2024-12-18 08:24:29 +0530

    Graph Color Changed

--- src/Components/GraphArea/GraphArea.jsx
+++ src/Components/GraphArea/GraphArea.jsx
@@ -1,83 +1,93 @@
 import React from "react";
-import {Line} from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 import {
-    Chart,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    Filler,
-    TimeScale
-} from 'chart.js';
-import 'chartjs-adapter-date-fns';
+  Chart,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+  TimeScale,
+} from "chart.js";
+import "chartjs-adapter-date-fns";
 import "./GraphArea.css";
 
 // Register the required components
-Chart.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler, TimeScale);
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+  TimeScale
+);
 
-function GraphArea({data}) {
-    console.log("GraphArea data:", data); // Debugging log
+function GraphArea({ data }) {
+  console.log("GraphArea data:", data); // Debugging log
 
-    const chartData = {
-        labels: data.map((item) => new Date(item.TimeStamp)), // Parse TimeStamp as Date
-        datasets: [
-            {
-                label: "Demand (Pred)",
-                data: data.map((item) => item["Demand(Pred)"]),
-                borderColor: "#f0f3bd",
-                backgroundColor: "#f0f3bd",
-                fill: false,
-            },
-            {
-                label: "Demand (Actual)",
-                data: data.map((item) => item["Demand(Actual)"]),
-                borderColor: "#028090",
-                backgroundColor: "#028090",
-                fill: true,
-            }
-        ],
-    };
+  const chartData = {
+    labels: data.map((item) => new Date(item.TimeStamp)), // Parse TimeStamp as Date
+    datasets: [
+      {
+        label: "Demand (Pred)",
+        data: data.map((item) => item["Demand(Pred)"]),
+        borderColor: "#C73B2F",
+        backgroundColor: "#C73B2F",
+        fill: false,
+      },
+      {
+        label: "Demand (Actual)",
+        data: data.map((item) => item["Demand(Actual)"]),
+        borderColor: "#028090",
+        backgroundColor: "#028090",
+        fill: true,
+      },
+    ],
+  };
 
-    const options = {
-        responsive: true,
-        scales: {
-            x: {
-                type: 'time',
-                time: {
-                    tooltipFormat: 'MM dd yyyy ',
-                    displayFormats: {
-                        minute: 'MMM dd, yyyy HH:mm'
-                    }
-                },
-                title: {
-                    display: true,
-                    text: 'Time'
-                }
-            }
+  const options = {
+    responsive: true,
+    scales: {
+      x: {
+        type: "time",
+        time: {
+          tooltipFormat: "MM dd yyyy ",
+          displayFormats: {
+            minute: "MMM dd, yyyy HH:mm",
+          },
         },
-        plugins: {
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: true,
-                text: 'Demand (Actual) vs Demand (Pred)',
-            },
+        title: {
+          display: true,
+          text: "Time",
         },
-        animation: {
-            duration: 100,
-            easing: 'easeInOutQuad',
-        },
-    };
+      },
+    },
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Demand (Actual) vs Demand (Pred)",
+      },
+    },
+    animation: {
+      duration: 100,
+      easing: "easeInOutQuad",
+    },
+  };
 
-    return (
-        <div className="graph-area">
-            <Line data={chartData} options={options}/>
-        </div>
-    );
+  return (
+    <div className="graph-area">
+      <Line data={chartData} options={options} />
+    </div>
+  );
 }
 
 export default GraphArea;
